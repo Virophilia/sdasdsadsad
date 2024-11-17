86f9a3d6ceeadf4652106f585adecadcc80db3a4
Virophilia <sijos.kontakt@gmail.com>
2024-11-17 11:01:07 +0100

    Add files via upload

---
+++ filtering.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title('Ahoy!')
+hide_st_style = """
+            <style>
+            #MaiMenU {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
